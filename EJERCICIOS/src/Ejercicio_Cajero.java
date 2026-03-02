dcfc1eb209e18c0b6d9171f5572ffa58543d22f1
asonleonardo-max <asonleonardo@gmail.com>
2026-03-02 06:56:32 -0500

    Arrays

---
+++ EJERCICIOS/src/Ejercicio_Cajero.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/*El Cajero del Supermercado (Vectores y Sumatorias)
+Objetivo: Llenar un vector manualmente y procesar la información.
+El Problema: El usuario debe ingresar los precios de 5 productos que compró en el mercado para calcular el total.
+区
+D
+Instrucciones:
+1. Crea un vector doble[] precios
+= new double[5]; •
+2. Usa un ciclo for y Scanner para pedirle al usuario que ingrese los 5 precios uno por uno.
+3. Tarea A: Calcula la suma total de los precios ingresados.
+4. Tarea B: Encuentra cuál fue el precio más alto que se ingresó y muestralo.
+5. Tarea C: Si el total supera los 20.000, muestra un mensaje: "¡Tienes un descuento de 2.000!". Resta ese valor y
+muestra el total final a pagar.*/
+
+public class Ejercicio_Cajero {
+    
+    double [] precios = new double[5];
+
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.println("Ingrese los 5 precios de cada uno: ");
+
+    for double i 
+
+}
